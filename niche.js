a51e5686ca0808b4819b61ee81b3241fc9d2d96a
eva415 <38572941+eva415@users.noreply.github.com>
2019-07-02 15:09:18 -0700

    Create niche.js

---
+++ niche.js
@@ -0,0 +1,43 @@
+function showHideStephen() {
+  var x = document.getElementById('stephen');
+  if (x.style.display == "none") x.style.display = "block";
+  else x.style.display = "none";
+  var elem = document.getElementById('showHideStephen');
+  if (elem.innerHTML == "more") elem.innerHTML = "less";
+  else elem.innerHTML = "more";
+}
+
+function showHidePlants() {
+  var x = document.getElementById('plants');
+  if (x.style.display == "none") x.style.display = "block";
+  else x.style.display = "none";
+  var elem = document.getElementById('showHidePlants');
+  if (elem.innerHTML == "more") elem.innerHTML = "less";
+  else elem.innerHTML = "more";
+}
+function showHideLiam() {
+  var x = document.getElementById('liam');
+  if (x.style.display == "none") x.style.display = "block";
+  else x.style.display = "none";
+  var elem = document.getElementById('showHideLiam');
+  if (elem.innerHTML == "more") elem.innerHTML = "less";
+  else elem.innerHTML = "more";
+}
+
+function showHideLove() {
+  var x = document.getElementById('love');
+  if (x.style.display == "none") x.style.display = "block";
+  else x.style.display = "none";
+  var elem = document.getElementById('showHideLove');
+  if (elem.innerHTML == "more") elem.innerHTML = "less";
+  else elem.innerHTML = "more";
+}
+
+function showHidePeople() {
+  var x = document.getElementById('people');
+  if (x.style.display == "none") x.style.display = "block";
+  else x.style.display = "none";
+  var elem = document.getElementById('showHidePeople');
+  if (elem.innerHTML == "more") elem.innerHTML = "less";
+  else elem.innerHTML = "more";
+}
